fdae9c00a59baf4bcdd8df0ddf91537d4c3e8066
Anurag Singh <sa.anuragsingh@gmail.com>
2021-04-17 22:30:39 +0530

    3: basic shapes

---
+++ sketch.js
@@ -0,0 +1,41 @@
+const CRYSTAL_SIZE = 500
+const SIDES = 6
+let PALETTE = []
+
+function setup() {
+  createCanvas(530, 530, SVG)
+
+  PALETTE = [
+    color(255, 52, 154), //pink
+    color(4, 0, 152)  //blue
+  ]
+
+  noLoop()
+  angleMode(DEGREES)
+  rectMode(CENTER)
+}
+
+function draw() {
+  // background('teal')
+  testLines()
+}
+
+function testLines() {
+  noFill()
+  stroke(PALETTE[0])
+
+  push()
+		translate(width/2, height/2)
+
+    ellipse(0, 0, CRYSTAL_SIZE, CRYSTAL_SIZE)
+    
+    stroke(PALETTE[1])
+
+    const angle = 360/SIDES
+
+    for(let i=0; i<SIDES; i++) {
+      line(0, 0, CRYSTAL_SIZE/2, 0)
+      rotate(angle)
+    }
+  pop()
+}
